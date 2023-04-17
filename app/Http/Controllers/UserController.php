86704d4e28c93a058ababbe67153149ffbbdc4cb
Nguyen Cao Ky <ky.nguyencao23@gmail.com>
2023-04-17 13:57:29 +0700

    user interface

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,7 +21,7 @@ class UserController extends Controller
     public function index(): View|\Illuminate\Foundation\Application|Factory|Application
     {
         return view('users.index', [
-            'title' => 'artStock',
+            'title' => 'artStock'
         ]);
     }
 
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,22 +1,48 @@
+@php use Illuminate\Support\Facades\Auth; @endphp
 @extends('layout.welcome')
 @push('css')
-    <link rel="stylesheet" href="{{ asset('/css/user.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/user.css?v=3') }}">
 @endpush
 @section('content')
-    <div id="navbar" class="container-fluid row flex-box" style="background-color: black">
-        <div class="col-4">
-            <a href="#">
+    {{ $user = Auth::user() }}
+    <div id="navbar" class="container-fluid row bg-black noMargin">
+        <div class="col-2">
+            <a href="{{ route('user.index') }}">
                 <img src="{{ asset('/assets/logo/cover-removebg-preview (1).png') }}" alt="" height="90px">
             </a>
         </div>
-        <div class="col-6">
-            <div id="searchbar">
+        <div class="col-8 flex-box align-center">
+            <div id="searchbar" class="full-width">
                 <form class="d-flex" role="search">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success bg-success color_white btn-hover" type="submit">Search</button>
+                    <button class="btn btn-outline-white color_white btn-hover" type="submit">Search</button>
                 </form>
-                <p class="text-center color_white letter-space">dipping in the cave of art..</p>
+            </div>
+        </div>
+        <div class="col-2 text-white">
+            <div class="row container-fluid align-center">
+                <div class="col-8">
+                    Welcome
+                    <br>
+                    <a href="{{ route('user.logout') }}">
+                        LOG OUT
+                    </a>
+                </div>
+                <div id="user_avatar" class="col-4 container-fluid"></div>
             </div>
         </div>
     </div>
 @endsection
+@push('js')
+    <script>
+        window.addEventListener("pageshow", function (event) {
+            let historyTraversal = event.persisted ||
+                (typeof window.performance != "undefined" &&
+                    window.performance.getEntriesByType("navigation")[0].type === "back_forward");
+            if (historyTraversal) {
+                // Handle page restore.
+                window.location.reload();
+            }
+        });
+    </script>
+@endpush
