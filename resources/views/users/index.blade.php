4b9396254f568e0bf2fb2b683c63e85a947939bb
Nguyen Cao Ky <ky.nguyencao23@gmail.com>
2023-05-04 11:14:29 +0700

    file optimization

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -18,22 +18,7 @@
             </div>
         </div>
         <div class="col-2 text-white d-flex align-items-center">
-            <div class="row container-fluid">
-                <div class="col-8">
-                    @php
-                        $user = current_user();
-                    @endphp
-                    Welcome, {{ $user->getAttribute('first_name') }}
-                    <br>
-                    <a href="{{ route('user.logout') }}">
-                        LOG OUT
-                    </a>
-                </div>
-                <div class="col-4 container-fluid">
-                    <img id="user_avatar" src="https://www.yarrah.com/media/81/9c/f0/1644837814/Yarrah-cat-meow.jpg"
-                         alt="">
-                </div>
-            </div>
+            @include('users.user_menu')
         </div>
     </div>
     <div class="row no-margin">
@@ -52,6 +37,9 @@
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">About me</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">New Post</a>
                     </li>
                 </ul>
             </div>
