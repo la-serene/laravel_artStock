13c75d72bda5a1ae3ca0e1c31f446d8c1d23bee0
Nguyen Cao Ky <ky.nguyencao23@gmail.com>
2023-06-19 04:05:16 +0700

    icon display fix

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -4,17 +4,17 @@
             <div class="col-md-6 mb-3">
                 <h4>Contact Info</h4>
                 <ul class="list-unstyled">
-                    <li><i class="fa ti-mobile"></i> 0325373390</li>
-                    <li><i class="fa ti-email"></i> ky.NguyenCao23@gmail.com</li>
+                    <li><i class="ti-mobile"></i> 0325373390</li>
+                    <li><i class="ti-email"></i> ky.NguyenCao23@gmail.com</li>
                 </ul>
             </div>
             <div class="col-md-6">
                 <h4>Follow Me</h4>
                 <ul class="list-unstyled social">
-                    <li><a href="https://www.facebook.com/ky.nekz" target="_blank"><i class="fa ti-facebook"></i></a></li>
-                    <li><a href="https://www.instagram.com/ichbin_ky/" target="_blank"><i class="fa ti-instagram"></i></a></li>
-                    <li><a href="https://www.linkedin.com/in/k%E1%BB%B3-nguy%E1%BB%85n-84918126a/" target="_blank"><i class="fa ti-linkedin"></i></a></li>
-                    <li><a href="https://github.com/kyNguyenCao" target="_blank"><i class="fa ti-github"></i></a></li>
+                    <li><a href="https://www.facebook.com/ky.nekz" target="_blank"><i class="ti-facebook"></i></a></li>
+                    <li><a href="https://www.instagram.com/ichbin_ky/" target="_blank"><i class="ti-instagram"></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/k%E1%BB%B3-nguy%E1%BB%85n-84918126a/" target="_blank"><i class="ti-linkedin"></i></a></li>
+                    <li><a href="https://github.com/kyNguyenCao" target="_blank"><i class="ti-github"></i></a></li>
                 </ul>
             </div>
         </div>
